f9c61687b08eb51a563a069779d29ed7272b12c8
administrator <administrator@7256.local>
2020-07-26 11:44:37 -0700

    add disk utilization

--- installers/install-osx.bash
+++ installers/install-osx.bash
@@ -3,7 +3,7 @@ set -e
 
 
 # Create a user for the script
-sysadminctl -addUser _cwpublisher -admin
+#sysadminctl -addUser _cwpublisher -admin
 
 
 # Install dependencies
@@ -16,23 +16,23 @@ mkdir -p /opt/aws/amazon-cloudwatch-publisher/etc/
 mkdir -p /opt/aws/amazon-cloudwatch-publisher/logs/
 cp amazon-cloudwatch-publisher /opt/aws/amazon-cloudwatch-publisher/bin/
 cp configs/amazon-cloudwatch-publisher-osx.json /opt/aws/amazon-cloudwatch-publisher/etc/amazon-cloudwatch-publisher.json
-chown -R _cwpublisher: /opt/aws/amazon-cloudwatch-publisher
+chown -R administrator: /opt/aws/amazon-cloudwatch-publisher
 chmod -R u+rwX,g-rwx,o-rwx /opt/aws/amazon-cloudwatch-publisher
 
 
 # Write configuration file details
-read -p "Region: " region
-read -p "Account ID: " account_id
-read -p "User Pool ID: " user_pool_id
-read -p "Identity Pool ID: " identity_pool_id
-read -p "App Client ID: " app_client_id
-read -p "Password: " password
-sed -i "s/REGION/$region/" /opt/aws/amazon-cloudwatch-publisher/etc/amazon-cloudwatch-publisher.json
-sed -i "s/ACCOUNT_ID/$account_id/" /opt/aws/amazon-cloudwatch-publisher/etc/amazon-cloudwatch-publisher.json
-sed -i "s/USER_POOL_ID/$user_pool_id/" /opt/aws/amazon-cloudwatch-publisher/etc/amazon-cloudwatch-publisher.json
-sed -i "s/IDENTITY_POOL_ID/$identity_pool_id/" /opt/aws/amazon-cloudwatch-publisher/etc/amazon-cloudwatch-publisher.json
-sed -i "s/APP_CLIENT_ID/$app_client_id/" /opt/aws/amazon-cloudwatch-publisher/etc/amazon-cloudwatch-publisher.json
-sed -i "s/PASSWORD/$password/" /opt/aws/amazon-cloudwatch-publisher/etc/amazon-cloudwatch-publisher.json
+#read -p "Region: " region
+#read -p "Account ID: " account_id
+#read -p "User Pool ID: " user_pool_id
+#read -p "Identity Pool ID: " identity_pool_id
+#read -p "App Client ID: " app_client_id
+#read -p "Password: " password
+#sed -i "s/REGION/$region/" /opt/aws/amazon-cloudwatch-publisher/etc/amazon-cloudwatch-publisher.json
+#sed -i "s/ACCOUNT_ID/$account_id/" /opt/aws/amazon-cloudwatch-publisher/etc/amazon-cloudwatch-publisher.json
+#sed -i "s/USER_POOL_ID/$user_pool_id/" /opt/aws/amazon-cloudwatch-publisher/etc/amazon-cloudwatch-publisher.json
+#sed -i "s/IDENTITY_POOL_ID/$identity_pool_id/" /opt/aws/amazon-cloudwatch-publisher/etc/amazon-cloudwatch-publisher.json
+#sed -i "s/APP_CLIENT_ID/$app_client_id/" /opt/aws/amazon-cloudwatch-publisher/etc/amazon-cloudwatch-publisher.json
+#sed -i "s/PASSWORD/$password/" /opt/aws/amazon-cloudwatch-publisher/etc/amazon-cloudwatch-publisher.json
 
 
 # Write the launch daemon configuration file
@@ -41,7 +41,7 @@ cat << EOF > /Library/LaunchDaemons/com.amazonaws.amazon-cloudwatch-publisher.pl
 <!DOCTYPE plist PUBLIC "-//Apple//DTD PLIST 1.0//EN" "http://www.apple.com/DTDs/PropertyList-1.0.dtd">
 <plist version="1.0">
 <dict>
-  <key>UserName</key><string>_cwpublisher</string>
+  <key>UserName</key><string>administrator</string>
   <key>KeepAlive</key><true/>
   <key>RunAtLoad</key><true/>
   <key>Label</key><string>com.amazonaws.amazon-cloudwatch-publisher</string>
